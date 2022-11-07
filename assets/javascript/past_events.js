ab178e9542f9672118ec8bbf52c85ef5dede9cbb
Senkidev <dante.vilchesc@gmail.com>
2022-11-07 00:32:19 -0300

    filtro search y checkbox a upcoming y past events

--- assets/javascript/past_events.js
+++ assets/javascript/past_events.js
@@ -1,8 +1,8 @@
 let cards = document.getElementById("cards");
 let fragment = document.createDocumentFragment();
 let fragment2 = document.createDocumentFragment();
+let containerCheck = document.getElementById("containerCheck");
 const data = info.events;
-
 let categories = [];
 
 //Crear checkbox
@@ -15,17 +15,20 @@ data.forEach((element) => {
 categories.sort().forEach((category) => {
 	let label = document.createElement("label");
 	label.innerHTML += `<input type="checkbox"
-									name="${category}"								
+									value="${category}"								
 								/>${category}`;
 	fragment2.appendChild(label);
 });
 containerCheck.appendChild(fragment2);
 
-data.forEach((evento) => {
-	if (evento.date < info.currentDate) {
-		let div = document.createElement(`div`);
-		div.classList.add(`card`);
-		div.innerHTML += `<img
+//crear cartas
+function createCard(array) {
+	cards.innerHTML = "";
+	array.forEach((evento) => {
+		if (evento.date < info.currentDate) {
+			let div = document.createElement(`div`);
+			div.classList.add(`card`);
+			div.innerHTML += `<img
 						src="${evento.image}"
 						class="card-img-top"
 						alt="Image of ${evento.name}"
@@ -44,8 +47,49 @@ data.forEach((evento) => {
 							>More info...</a
 						>
 					</div>`;
-		fragment.appendChild(div);
-	}
-});
+			fragment.appendChild(div);
+		}
+	});
+	cards.appendChild(fragment);
+}
 
-cards.appendChild(fragment);
+// filtro searchbar
+
+let searchBar = document.querySelector("input[type=search]");
+
+function filterSearch() {
+	let filterInput = searchBar.value.toLowerCase();
+	let aux = [];
+	filterCheck().forEach((element) => {
+		if (element.name.toLocaleLowerCase().includes(filterInput)) {
+			aux.push(element);
+		}
+	});
+	createCard(aux);
+}
+
+// filtro Check box
+
+function filterCheck() {
+	let checkboxes = document.querySelectorAll("input[type=checkbox]");
+	let arrayCheckboxes = Array.from(checkboxes);
+	let checkboxesSeleccionados = arrayCheckboxes.filter(
+		(checkbox) => checkbox.checked == true
+	);
+	let allChecks = checkboxesSeleccionados.map((checkbox) => checkbox.value);
+	let eventFilteredByCategory = [];
+	if (allChecks.length == 0) {
+		eventFilteredByCategory = data;
+		createCard(data);
+	} else {
+		eventFilteredByCategory = data.filter((evento) =>
+			allChecks.includes(evento.category)
+		);
+		createCard(eventFilteredByCategory);
+	}
+
+	return eventFilteredByCategory;
+}
+
+containerCheck.addEventListener("change", filterCheck); // escucha check y modifica las card dependiendo del seleccionado
+createCard(data);
--- assets/javascript/upcoming_events.js
+++ assets/javascript/upcoming_events.js
@@ -1,8 +1,8 @@
 let cards = document.getElementById("cards");
+let containerCheck = document.getElementById("containerCheck");
 let fragment = document.createDocumentFragment();
 let fragment2 = document.createDocumentFragment();
 const data = info.events;
-let date = info.currentDate.split("-");
 let categories = [];
 
 //Crear checkbox
@@ -15,19 +15,20 @@ data.forEach((element) => {
 categories.sort().forEach((category) => {
 	let label = document.createElement("label");
 	label.innerHTML += `<input type="checkbox"
-									name="${category}"								
+									value="${category}"								
 								/>${category}`;
 	fragment2.appendChild(label);
 });
 containerCheck.appendChild(fragment2);
 
 // Crear cards
-data.forEach((evento) => {
-	let parts = evento.date.split("-");
-	if (Number(parts[0]) >= Number(date[0])) {
-		let div = document.createElement(`div`);
-		div.classList.add(`card`);
-		div.innerHTML += `<img
+function createCard(array) {
+	cards.innerHTML = "";
+	array.forEach((evento) => {
+		if (evento.date > info.currentDate) {
+			let div = document.createElement(`div`);
+			div.classList.add(`card`);
+			div.innerHTML += `<img
 						src="${evento.image}"
 						class="card-img-top"
 						alt="Image of ${evento.name}"
@@ -47,8 +48,49 @@ data.forEach((evento) => {
 						>
 						
 					</div>`;
-		fragment.appendChild(div);
-	}
-});
+			fragment.appendChild(div);
+		}
+	});
+	cards.appendChild(fragment);
+}
 
-cards.appendChild(fragment);
+// filtro searchbar
+
+let searchBar = document.querySelector("input[type=search]");
+
+function filterSearch() {
+	let filterInput = searchBar.value.toLowerCase();
+	let aux = [];
+	filterCheck().forEach((element) => {
+		if (element.name.toLocaleLowerCase().includes(filterInput)) {
+			aux.push(element);
+		}
+	});
+	createCard(aux);
+}
+
+// filtro Check box
+
+function filterCheck() {
+	let checkboxes = document.querySelectorAll("input[type=checkbox]");
+	let arrayCheckboxes = Array.from(checkboxes);
+	let checkboxesSeleccionados = arrayCheckboxes.filter(
+		(checkbox) => checkbox.checked == true
+	);
+	let allChecks = checkboxesSeleccionados.map((checkbox) => checkbox.value);
+	let eventFilteredByCategory = [];
+	if (allChecks.length == 0) {
+		eventFilteredByCategory = data;
+		createCard(data);
+	} else {
+		eventFilteredByCategory = data.filter((evento) =>
+			allChecks.includes(evento.category)
+		);
+		createCard(eventFilteredByCategory);
+	}
+
+	return eventFilteredByCategory;
+}
+
+containerCheck.addEventListener("change", filterCheck); // escucha check y modifica las card dependiendo del seleccionado
+createCard(data);
